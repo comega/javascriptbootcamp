c773d330c36182d64a2e397fe1642647031e3595
comega <mohamadzade68@gmail.com>
2019-03-13 13:24:13 +0330

    second section

---
+++ es6/main.js
@@ -0,0 +1,92 @@
+
+
+
+// let name="ali";
+
+// var family="moha";
+
+
+// // const age = 27;
+
+// age=28;
+
+// // let name = "amir";
+
+// // let i =100;
+
+// // console.log(i);
+
+// // for(let i=0;i<10;i++){
+// //     console.log(i);
+// // }
+
+// // console.log(i);
+
+// const obj1 = {age:23};
+
+// obj1.lastname="family";
+
+// console.log(obj1);
+
+// const obj2 = obj1;
+
+// obj1.name="ali";
+
+// console.log(obj2);
+
+
+
+// mySum = function (a,b){
+
+//     return a+b;
+
+// }
+
+
+// mysum = a => { a**2;}
+
+// myBtn = document.getElementById('btn').addEventListener('click', e =>{ console.log(e); });
+
+
+
+
+
+// function Person(name,lastname){
+//     this.name = name;
+//     this.lastname=lastname
+// }
+
+// console.log(new Person("ali","moha"));
+
+
+// class Person {
+//     constructor(name,family){
+//         this.name = name;
+//         this.family = family;
+//     }
+// }
+
+// let mohsen = new Person("mohsen","ker..");
+
+// console.log(mohsen);
+
+
+
+// setTimeout(()=>{
+//     console.log("salma");
+// },1000);
+
+
+// /// Ajax ////
+
+
+
+// /////////////
+// data;
+
+// var obj3 = '{"name":"ali"}';
+
+
+
+
+
